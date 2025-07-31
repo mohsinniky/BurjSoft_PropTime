a1ca79e9c562f5fd586e5cf3976634380c12e8a5
mohsinniky <Mohsinraza490490@gmail.com>
2025-07-31 21:39:48 +0500

    Toast Remaining

--- Day4 Onwards/js/your-scripts.js
+++ Day4 Onwards/js/your-scripts.js
@@ -3,6 +3,23 @@ const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstra
 
 
 
-// Initialize all toasts on the page
-const toastTriggerList = document.querySelectorAll('[data-bs-toggle="toast"]');
-const toastList = [...toastTriggerList].map(toastEl => new bootstrap.Toast(toastEl));
+// Initialize toasts with custom options
+const toastElList = document.querySelectorAll('.toast');
+const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl, {
+  autohide: false,       // Disable auto-hiding
+  delay: 5000            // Hide after 5 seconds (if autohide is true)
+}));
+
+// Show toast on button click
+document.getElementById('liveToastBtn').addEventListener('click', function () {
+    const toastLive = document.getElementById('liveToast');
+    const toast = bootstrap.Toast.getOrCreateInstance(toastLive);
+    toast.show();
+});
+
+// Show toast on button click
+document.getElementById('customToastBtn').addEventListener('click', function () {
+    const toastElement = document.getElementById('customToast');
+    const toast = bootstrap.Toast.getOrCreateInstance(toastElement);
+    toast.show();
+});
