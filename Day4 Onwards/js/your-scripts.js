5fa1272916df2028156634c01c4a3a48b7431ab9
mohsinniky <Mohsinraza490490@gmail.com>
2025-07-31 20:55:40 +0500

    Updated till Spinner

--- Day4 Onwards/js/your-scripts.js
+++ Day4 Onwards/js/your-scripts.js
@@ -1,2 +1,8 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+
+// Initialize all toasts on the page
+const toastTriggerList = document.querySelectorAll('[data-bs-toggle="toast"]');
+const toastList = [...toastTriggerList].map(toastEl => new bootstrap.Toast(toastEl));
