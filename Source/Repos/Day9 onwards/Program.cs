f430773ae352ca74d63835241119418eb849db9f
mohsinniky <Mohsinraza490490@gmail.com>
2025-08-08 13:27:36 +0500

    Updated , C# started

---
+++ Source/Repos/Day9 onwards/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello, This Is Mohsin Writing His First Program!");
+Console.Write("Using Write() to write this line lets see what happens! \n");
+Console.Write("Using Write() to write this 2nd line lets see what happens!");
+                                                  
+//Below is the code of using Read() and ReadLine()
+Console.WriteLine("Enter your Age");
+string age = Console.ReadLine();
+int rdExample = Console.Read();
+Console.WriteLine(rdExample);
+
+Console.WriteLine("Your entered Age is:  " + age + "   " + rdExample);
+
+
+
+
+
+
+
