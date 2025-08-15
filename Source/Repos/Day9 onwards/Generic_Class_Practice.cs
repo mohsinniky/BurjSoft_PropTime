b187c7a39ec3dbb46c5102d9f4bd4548d3871458
mohsinniky <Mohsinraza490490@gmail.com>
2025-08-15 20:06:52 +0500

    Updated Till The End Of Day 15

---
+++ Source/Repos/Day9 onwards/Generic_Class_Practice.cs
@@ -0,0 +1,87 @@
+﻿////using System;
+////using System.Collections.Generic;
+////using System.Linq;
+////using System.Text;
+////using System.Threading.Tasks;
+
+////namespace Day9_onwards
+////{
+////    /* C# Generics:
+//// * create a single class or method that can be used with different types of data.
+//// * it is used to create an class of any datatype
+//// * to define  a generic Class we use " < > " 
+//// * With the help of generics in C#, we can write code that will work with different types of data
+//// * The collections framework uses the concept of generics
+//// * 
+//// * 
+//// * Int the below example the letter T inside < > is called type Parameter
+//// * while Createing the instance of the class , we specify the data type of the object which replaces the Type Parameter
+
+//// for specify T type like below we set 'class' keyword
+//public class Generic<T> where T : class
+//{
+
+//}
+
+//public class GenericWithParameters<TKey, TValue>
+//{
+
+//}
+
+//public class Room
+//{
+
+//}
+
+
+
+
+//// */
+////    public class Student<T> 
+////    {
+////        //Defining The Constructor
+////        //public T Data;
+////        //public Student(T Data) 
+////        //{ 
+////        //    this.Data = Data;
+////        //    Console.WriteLine("Data Fetched: {0} ", this.Data);
+////        //}
+
+////        // define a generics method that displays the passed data  
+////        public void displayData(T data)
+////        {
+////            Console.WriteLine("The data passed is: " + data);
+////        }
+
+////        //// defining a generics method that returns T type value    
+////        //public T displayData(T data)
+////        //{
+////        //    return data;
+////        //}
+
+////    }
+//public static class Generic_Class_Practice
+//{
+
+//    static void Main(string[] args)
+//    {
+//        Generic<string> obj = new Generic<string>();
+//        Generic<Room> roomObj = new Generic<Room>();
+
+//        GenericWithParameters<string, int> withTwoParamObj = new GenericWithParameters<string, int>();
+
+
+//        //Generic Class Object Instance Creation with Specified Datatypes
+//        //Student<string> studentName = new Student<string>("Name");
+//        //Student<int> studentId = new Student<int>(11);
+
+//        //Generic Class Instance without parameterized Constructor;
+//        //Student<string> studentName = new Student<string>();
+//        //Student<int> studentId = new Student<int>();
+//        //studentName.displayData("MohsinRaza");
+//        //studentId.displayData(19);
+
+//    }
+
+//}
+////}
--- Source/Repos/Day9 onwards/Program.cs
+++ Source/Repos/Day9 onwards/Program.cs
@@ -1,41 +1,146 @@
 ﻿using Oops;
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Enumeration;
 
 namespace Day9_onwards
 {
-    /* C# Generics:
-     * create a single class or method that can be used with different types of data.
-     * it is used to create an class of any datatype
-     * to define  a generic Class we use " < > " 
-     * 
-     * 
-     * 
-     * Int the below example the letter T inside < > is called type Parameter
-     * while Createing the instance of the class , we specify the data type of the object which replaces the Type Parameter
+    /* C# Collections:
+     * Collections are classes that provide an easy way to work with a g
+     * System.Collections.Generic:
+     *      classes provide the implementation of strongly typed entities like lists, stacks etc
+     *      to create a generic collection.
+     *      in this we store , Type Compatible Data elements
+     *      Storing Different type of elements not allowed here 
+     *      Classes Under this: List Stack SortedList Queue
+     * System.Collections: 
+     *      To create a non Generic Colloection
+     *      Using this we can create classes where we can add data elements of multiple data types
+     *      Classes Under this: ArrayList Hashtable
+     * System.Collections.Concurrent:
+     *      Classes that help to achieve thread-safe code
+     *      Thread-Safe Code:
+     *          There can be situations when multiple threads are trying to execute the same piece of code
+     *          The code is said to be "thread-safe" if it can be executed correctly irrespective of multiple threads accessing concurrently.
+     *      These are the classes to be used when multiple threads are accessing the collection
+     *      Classes Under This: ConcurrentStack<T> ConcurrentQueue<T> ConcurrentDictionary<TKey,TValue>
      */
-    public class Student<T>
-    {
-        //Defining The Constructor
-        public T Data;
-        public Student(T Data) 
-        { 
-            this.Data = Data;
-            Console.WriteLine("Data Fetched: {0} ", this.Data);
-        }
 
-    }
+
+
     public static class Program
     {
-        
+
         static void Main(string[] args)
         {
-            //Generic Class Object Instance Creation with Specified Datatypes
-            Student<string> studentName = new Student<string>("Name");
-            Student<int> studentId = new Student<int>(11);
+            //Creating A list  
+            List<string> subjects = new List<string>() { "English", "Math" };
+
+
+            Console.WriteLine("Subject 1:  {0}.", subjects[0]);
+            Console.WriteLine("Subject 2:  {0}.", subjects[1]);
+            Console.WriteLine("Subject List Items Count:  {0}", subjects.Count);
+            Console.WriteLine("Subject List Items Count:  {0}", subjects.Capacity);
+
+            // Adding Items
+            subjects.Add("Physics");
+            subjects.Add("Chemistry");
+            subjects.Add("Biology");
+
+            // Looping thorugh the list
+            foreach(var subject in subjects)
+            {
+                Console.WriteLine($"Subject Number {subject}");
+            }
+
+            Console.WriteLine("Subject List Items Count Before Removing:  {0}", subjects.Count);
+
+
+            // Removing Item
+            subjects.Remove("Chemistry");
+
+            // remove by specific index
+            subjects.RemoveAt(2);
+
+            // Insert Items
+            subjects.Insert(2, "Geography");
+
+            // Looping thorugh the list
+            for (int i = 0; i < subjects.Count; i++)
+            {
+                Console.WriteLine("Subject Number {0}:  {1}", i, subjects[i]);
+            }
+
+                                                                                        //Methods
+            // AddRange
+            subjects.AddRange(subjects);
+            // AsReadOnly
+            IList<string> readlist = subjects.AsReadOnly();
+            //readlist.Add("testing"); // this causes Exception Error
+            // Random Integer List
+            List<int> randomInts = new List<int>() {1,4,51,62,3,2,8,9,11,15,53};
+            // Sort
+            randomInts.Sort();
+            //BinarySearch
+            Console.WriteLine("Binary Search:  "+randomInts.BinarySearch(51));
+            // Reverse
+            randomInts.Reverse();
+            //Contains
+            Console.WriteLine("Contains(4):  " + randomInts.Contains(4));
+            // IndexOf
+            Console.WriteLine("Index OF:  " + randomInts.IndexOf(4));
+            // Max
+            Console.WriteLine("Max:  " + randomInts.Max());
+            // Min
+            Console.WriteLine("Min:  " + randomInts.Min());
+            // Sum
+            Console.WriteLine("Sum:  " + randomInts.Sum());
+            // IndexOf
+            Console.WriteLine("IndexOF:  " + randomInts.IndexOf(51));
+            // LastIndexOf
+            Console.WriteLine("LastIndexOF:  " + randomInts.LastIndexOf(2));
+            // Find
+            Console.WriteLine("Find:  " + randomInts.Find(x => x > 50));
+            // FindAll
+            Console.WriteLine("FindAll:  " + randomInts.FindAll(x => x > 50));
+            // FindIndex
+            Console.WriteLine("FindIndex:  " + randomInts.FindIndex(x => x > 50));
+            // FindLastIndex
+            Console.WriteLine("FindLastIndex:  " + randomInts.FindLastIndex(x => x > 50));
+            // Exists
+            Console.WriteLine("Exists:  " + randomInts.Exists(x => x > 50));
+            // ToArray
+            int[] array = randomInts.ToArray();
+            // ToString
+            Console.WriteLine("ToString:  " + randomInts.ToString());
+            // Clear
+            //randomInts.Clear();
+
+            //NestedList
+            List<List<int>> nestedList = new List<List<int>>();
+            // Create inner lists
+            List<int> innerList1 = new List<int> { 1, 2, 3 };
+            List<int> innerList2 = new List<int> { 4, 5 };
+            // Add inner lists to the nested list
+            nestedList.Add(innerList1);
+            nestedList.Add(innerList2);
+            // ForEach
+            foreach (List<int> innerList in nestedList)
+            {
+                foreach (int item in innerList)
+                {
+                    Console.WriteLine("NestedListItem:  " + item);
+                }
+            }
+
+
+
+            
+
+
+
 
         }
-
     }
 }
