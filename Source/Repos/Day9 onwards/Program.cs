a3216dd52773844b666bfdc0dd2141a2cd72c08d
mohsinniky <Mohsinraza490490@gmail.com>
2025-08-15 16:29:10 +0500

    Updated till Day14

--- Source/Repos/Day9 onwards/Program.cs
+++ Source/Repos/Day9 onwards/Program.cs
@@ -5,86 +5,14 @@ using System.IO.Enumeration;
 
 namespace Day9_onwards
 {
-    //Public Keyword being used below is an access modifier, which defines a members of class Accessibility
-    //class childClass : ParentClass  // Inheraitance
-
-
-    /*           Abstraction:
-     *           - hiding certain details and showing only essential information
-     *           - can be achieved with either abstract classes or interfaces
-     *           Abstract class:
-     *           - a restricted class that cannot be used to create objects 
-     *              (to access it, it must be inherited from another class)
-     *           - Can contain fields, properties, and constructors
-     *           - During Inheritance Can't Use Public Keyword
-     *           Abstract Methods:
-     *           - methods without implementation
-     * 
-     * 
-     * 
-     * 
-     */
-    //Abstraction Method and inheriting
-    abstract class Animal
-    {
-        public abstract void animalSound();
-        public void sleep()
-        {
-            Console.WriteLine("Zzz");
-        }
-    }
-
-    class Dog : Animal
-    {
-        public override void animalSound()
-        {
-            Console.WriteLine("Woof Woof!");
-        }
-    }
-
-    //Practice of Interfaces
-    //It contains only method declarations(no implementation)
-    interface IFirstInterface
-    {
-        void myMethod();
-    }
-
-    interface ISecondInterface
-    {
-        void myOtherMethod();
-    }
-
-    // Inheriting
-    class DemoClass : IFirstInterface, ISecondInterface
-    {
-        public void myMethod()
-        {
-            Console.WriteLine("My First Method..");
-        }
-        public void myOtherMethod()
-        {
-            Console.WriteLine("My Second Method...");
-        }
-    }
-
 
     public static class Program
-    {    
-
+    {
+        
         static void Main(string[] args)
         {
 
-            Dog myDog = new Dog();
-            myDog.animalSound();
-            myDog.sleep();
-
-            DemoClass demoClass = new DemoClass(); 
-            demoClass.myMethod();
-            demoClass.myOtherMethod();
-
-
         }
-
 
     }
 }
--- Source/Repos/Day9 onwards/obj/Debug/net8.0/Day9 onwards.AssemblyInfo.cs
+++ Source/Repos/Day9 onwards/obj/Debug/net8.0/Day9 onwards.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day9 onwards")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb760e509c3c356787cdc680ca4b2d114fae4dfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3fc8200a75539065b36842d53fe52208598dc74")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day9 onwards")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day9 onwards")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
