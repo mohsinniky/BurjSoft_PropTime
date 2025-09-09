8a11491bf9048e50dca901bfa39077f1796f5070
mohsinniky <Mohsinraza490490@gmail.com>
2025-09-09 13:22:36 +0500

    Updated till ALter Table SQL

---
+++ MySQL/SQLQueryDay32.sql
@@ -0,0 +1,29 @@
+/*
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+Select * from Persons;
+Drop Table Persons;
+
+SELECT LastName, FirstName 
+INTO TestTable 
+FROM Persons;
+
+Select * from Persons;
+Select * from TestTable;
+
+ALTER TABLE Persons
+ADD Email varchar(255);
+Select * from Persons;
+
+
+
+
+*/
+
+
+
