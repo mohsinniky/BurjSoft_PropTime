62e29efcd89d6271871f33b537db219f07c99693
mohsinniky <Mohsinraza490490@gmail.com>
2025-10-16 18:16:07 +0500

    Updated miscellaneous stuuff

--- Source/Repos/MVC-Application/obj/Debug/net8.0/MVC-Application.AssemblyInfo.cs
+++ Source/Repos/MVC-Application/obj/Debug/net8.0/MVC-Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC-Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a348ac9806d108c34a0a9d53e27b900c596a859")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49b81c28c77276ee398a64812042f37352ab7b60")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC-Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC-Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
