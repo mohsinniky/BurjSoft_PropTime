e0d8161d6f92c652f01e1e60d4943ad3e326acf2
mohsinniky <Mohsinraza490490@gmail.com>
2025-10-17 19:01:28 +0500

    Updated till Some basic dapper queries

---
+++ Source/Repos/MVC-Application/Controllers/DappersController.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using MVC_Application.Models;
+
+namespace MVC_Application.Controllers
+{
+    public class DappersController : Controller
+    {
+        private readonly SqlConnection connection;
+        public DappersController()
+        {
+            string connectionString = "Server=DESKTOP-3OT71VJ;Database=SchoolManagement;User Id=sa;Password=123;TrustServerCertificate=true;Encrypt=false;";
+            connection = new SqlConnection(connectionString);
+        }
+        public IActionResult Index()
+        {
+            //Simple Query
+            string sqlQuery = "SELECT * FROM Courses";
+            var courses = connection.Query<Course>(sqlQuery).ToList();
+
+            //// Parameterized Query
+            //string sqlQuery2 = "SELECT * FROM Courses where CourseId = @param OR CourseId = @param2";
+            //var coursesNo1 = connection.Query<Course>(sqlQuery2, new { param = 1, param2 = 2 }).ToList();
+
+            var coursesByProcedure = connection.Query<Course>(
+                "GetAllCourses",
+                commandType: System.Data.CommandType.StoredProcedure
+            ).ToList();
+
+
+            return View();
+        }
+    }
+}
