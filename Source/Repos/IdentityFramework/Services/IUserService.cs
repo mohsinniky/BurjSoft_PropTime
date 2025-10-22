56243719b027a89f8299371cf45f0b66dc72a57b
mohsinniky <Mohsinraza490490@gmail.com>
2025-10-22 14:47:09 +0500

    Service Added

---
+++ Source/Repos/IdentityFramework/Services/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using IdentityFramework.Models;
+using IdentityFramework.ViewModels;
+using IdentityFramework.ViewModels.Users;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityFramework.Services
+{
+    public interface IUserService
+    {
+        Task<PagedResult<UserListItemViewModel>> GetUsersAsync(UserListFilterViewModel filter);
+        Task<(IdentityResult Result, Guid? UserId)> CreateAsync(UserCreateViewModel model);
+        Task<UserEditViewModel?> GetForEditAsync(Guid id);
+        Task<IdentityResult> UpdateAsync(UserEditViewModel model);
+        Task<UserDetailsViewModel?> GetDetailsAsync(Guid id);
+        Task<IdentityResult> DeleteAsync(Guid id);
+        Task<UserRolesEditViewModel?> GetRolesForEditAsync(Guid userId);
+        Task<IdentityResult> UpdateRolesAsync(Guid userId, IEnumerable<Guid> selectedRoleIds);
+    }
+}
